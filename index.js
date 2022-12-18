3323009bc142cda1285c213a08549a5cb0913a19
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-18 01:24:27 -0500

    Verified fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP, fetchCoordsByIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -19,3 +19,12 @@ const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 //   }
 
 // });
+
+fetchISSFlyOverTimes({latitude: 43.653226, longitude: -79.3831843}, (error, flyoverArray) => {
+  if (error) {
+    console.log("Error fetch details: ", error);
+    return;
+  } else {
+    console.log(flyoverArray);
+  }
+});
