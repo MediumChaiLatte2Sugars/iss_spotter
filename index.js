5ed55bef4b4933d4177f0af55f339657dfecfb8c
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-12-18 01:08:06 -0500

    implemented fetchCoordsByIP, linted files

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const { fetchMyIP } = require("./iss");
+const { fetchMyIP, fetchCoordsByIP } = require("./iss");
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("Error fetch details: ", error);
-    return;
-  } else {
-    console.log(ip);
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("Error fetch details: ", error);
+//     return;
+//   } else {
+//     console.log(ip);
+//   }
 
-});
+// });
+
+// fetchCoordsByIP( "42", (error, location) => {
+//   if (error) {
+//     console.log("Error fetch details: ", error);
+//     return;
+//   } else {
+//     console.log(location);
+//   }
+
+// });
--- iss.js
+++ iss.js
@@ -16,16 +16,54 @@ const fetchMyIP = function(callback) {
       return callback(error, null);
     }
 
-     // if non-200 status, assume server error
+    // if non-200 status, assume server error
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       return callback(Error(msg), null);
     }
 
-    const data = JSON.parse(body);
-    return callback(null, data["ip"]);
+    const ip = JSON.parse(body).ip;
+    return callback(null, ip);
 
   });
 };
 
-module.exports = { fetchMyIP };
+/**
+ * Makes a single API request to retrieve the user's location info.
+ * Input:
+ *   - A string of the user's IP address
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - An object containing the latiude and longitude coordinates of the user (null if error). Example: "{latitude: 43.212321 longitude: -41.412241}"
+ */
+const fetchCoordsByIP = function(ip, callback) {
+  // use request to fetch geo location from JSON API
+  request("http://ipwho.is/" + ip, (error, response, body) => {
+    
+    if (error) {
+      return callback(error, null);
+    }
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching location. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+
+    const data = JSON.parse(body);
+
+    // if error data received
+    if (data["success"] === false) {
+      const msg = `Houston, we have a problem! Error: Success status was false. Server says: ${data["message"]}`;
+      return callback(Error(msg), null);
+    }
+
+    return callback(null, { latitude: data.latitude, longitude: data.longitude });
+
+  });
+};
+
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
